4a79216147cec4d7b174bbf3425e851186040869
Kishor <kishor.kumar@evontech.com>
2024-03-27 01:54:24 +0530

    multi_company(1.2)

--- hr_multi_company/__manifest__.py
+++ hr_multi_company/__manifest__.py
@@ -21,14 +21,14 @@
 ###################################################################################
 {
     'name': 'Open HRMS Multi-Company',
-    'version': '16.0.1.1',
+    'version': '16.0.1.2',
     'summary': """Enables Multi-Company""",
     'description': 'This module enables multi company features',
     'category': 'Generic Modules/Human Resources',
     'author': 'Cybrosys Techno solutions,Open HRMS',
     'company': 'Cybrosys Techno Solutions',
     'website': "https://www.openhrms.com",
-    'depends': ['base', 'hr','hr_contract', 'hr_payroll_community', 'hr_expense', 'hr_attendance',],
+    'depends': ['base', 'hr','hr_contract', 'hr_payroll_community', 'hr_attendance',],
     'data': [
         'views/hr_company_view.xml',
         'views/multi_company_view.xml',
