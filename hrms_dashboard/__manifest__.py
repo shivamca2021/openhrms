ea05cef9895368412b3b2a637eaf42d7a8f4ae4b
Kishor <kishor.kumar@evontech.com>
2024-03-26 13:17:36 +0530

    Dashboard(1.55)

--- hrms_dashboard/__manifest__.py
+++ hrms_dashboard/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Open HRMS - HR Dashboard",
-    'version': '16.0.1.54',
+    'version': '16.0.1.55',
     'summary': """Open HRMS - HR Dashboard""",
     'description': """Open HRMS - HR Dashboard""",
     'category': 'Generic Modules/Human Resources',
@@ -12,7 +12,7 @@
     'website': "https://www.openhrms.com",
     'depends': ['base','hr', 'hr_holidays', 'hr_timesheet', 'hr_payroll_community', 'hr_contract',
                 'hr_attendance', 'hr_timesheet_attendance', 'calendar', 'project','stock_inventory',
-                'hr_recruitment', 'hr_resignation', 'event','spreadsheet_dashboard',
+                'hr_recruitment', 'event','spreadsheet_dashboard',
                 'hr_reward_warning'],
     'external_dependencies': {
         'python': ['pandas'],
