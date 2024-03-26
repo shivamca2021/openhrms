93f7494bb246904781647b1be982a9a4d2b09f27
Kishor <kishor.kumar@evontech.com>
2024-03-26 13:38:51 +0530

    Hrms_core(1.1)

--- ohrms_core/__manifest__.py
+++ ohrms_core/__manifest__.py
@@ -1,28 +1,6 @@
-# -*- coding: utf-8 -*-
-###################################################################################
-#    A part of Open HRMS Project <https://www.openhrms.com>
-#
-#    Cybrosys Technologies Pvt. Ltd.
-#    Copyright (C) 2022-TODAY Cybrosys Technologies (<https://www.cybrosys.com>).
-#    Author: Cybrosys (<https://www.cybrosys.com>)
-#
-#    This program is free software: you can modify
-#    it under the terms of the GNU Affero General Public License (AGPL) as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
-#
-###################################################################################
 {
     'name': 'Open HRMS Core',
-    'version': '16.0.1.0.0',
+    'version': '16.0.1.1',
     'summary': """Open HRMS Suit: It brings all Open HRMS modules""",
     'description': """Openhrms, Main module of Open HRMS, It brings all others into a single module, Payroll, Payroll Accounting,Expense,
                 Dashboard, Employees, Employee Document, Resignation, Salary Advance, Loan Management, Gratuity, Service Request, Gosi, WPS Report, Reminder, Multi Company, Shift Management, Employee History,
@@ -42,7 +20,6 @@
         'hr_attendance',
         'hr_holidays',
         'hr_payroll_community',
-        'hr_expense',
         'hr_leave_request_aliasing',
         'hr_timesheet',
         'oh_employee_creation_from_user',
