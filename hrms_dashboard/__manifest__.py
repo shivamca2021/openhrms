f173d14ac5e114a01d0a01814783b7fcf4a92923
Kishor <kishor.kumar@evontech.com>
2024-04-03 18:47:25 +0530

    Hrms_Dashboard(1.59A)

--- hrms_dashboard/__manifest__.py
+++ hrms_dashboard/__manifest__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Open HRMS - HR Dashboard",
-    'version': '16.0.1.59',
+    'version': '16.0.1.59A',
     'summary': """Open HRMS - HR Dashboard""",
     'description': """Open HRMS - HR Dashboard""",
     'category': 'Generic Modules/Human Resources',
